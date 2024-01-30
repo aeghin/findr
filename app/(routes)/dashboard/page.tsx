3c8c31e407d750ac0dc5c309819d9b6f67d00384
Edward Castaneda <aeghin7@gmail.com>
2024-01-29 23:58:03 -0600

    added zustand state management for modal

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -4,10 +4,22 @@
 import axios from 'axios';
 
 import { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
+import { Montserrat } from 'next/font/google';
 
+const mont = Montserrat({ weight: '600', subsets: ['latin']});
 
 const DashboardPage = () => {
+
 
     type Category = {
         id: number;
@@ -32,13 +44,29 @@ const DashboardPage = () => {
 
     return (
         <>
-            <div>
-                {categories.map((category) => (
-                    <div key={category.id}>
-                        {category.name}
-                    </div>
-                ))}
-            </div>
+            {categories.length === 0 ?
+                <div className='flex flex-col justify-center items-center min-h-screen'>
+                    <Card className='w-1/2 h-96 bg-gray-100 mb-36'>
+                        <CardHeader>
+                            <CardTitle className={`text-center ${mont.className}`}>Let's add your first category!</CardTitle>
+                            <CardDescription className={`text-center ${mont.className}`}>finance, crypto, sports..</CardDescription>
+                        </CardHeader>
+                        <div className='flex justify-center mt-12'>
+                        <Button className='w-1/3'>
+                            Create Category
+                        </Button>
+                        </div>
+                    </Card>
+                </div>
+                :
+                <div>
+                    {categories.map((category) => (
+                        <div key={category.id}>
+                            {category.name}
+                        </div>
+                    ))}
+                </div>
+            }
         </>
     )
 };
---
+++ store/create-cat-modal.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+interface CreateCategory {
+    isOpen: boolean,
+    onOpen: () => void;
+    onClose: () => void;
+};
+
+export const useCreateCategory = create<CreateCategory>((set) => ({
+    isOpen: false,
+    onClose: () => set({
+        isOpen: false;
+    }),
+    onOpen: () => set({
+        isOpen: true
+    }),
+}));
