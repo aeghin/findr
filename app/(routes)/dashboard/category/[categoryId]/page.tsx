cb45574f81a7b9901c7d4bca6cb936030ae8aa43
Edward Castaneda <aeghin7@gmail.com>
2024-03-08 23:07:35 -0600

    style: fixed button and category text alignment

--- app/(routes)/dashboard/category/[categoryId]/page.tsx
+++ app/(routes)/dashboard/category/[categoryId]/page.tsx
@@ -30,10 +30,12 @@ const CategoryPage: React.FC<CategoryPageProps> = ({ params }) => {
     return (
         <div className="p-4">
             <div className="mb-4">
-                <h1 className="text-xl font-semibold">{categoryName}</h1>
-                <button onClick={onOpen} className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
-                    Add Account
-                </button>
+                <div className="flex justify-between items-center">
+                    <h1 className="text-xl font-semibold">{categoryName}</h1>
+                    <button onClick={onOpen} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
+                        Add Account
+                    </button>
+                </div>
             </div>
             {isLoading ?
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
